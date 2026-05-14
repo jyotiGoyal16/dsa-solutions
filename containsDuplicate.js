58399ada8ceb52af1a4ce3e23548532761db5404
Jyoti Goyal <56450138+jyotiGoyal16@users.noreply.github.com>
2026-05-14 16:07:53 +0530

    Contains duplicate updated

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -5,8 +5,10 @@
  * @return {boolean}
  */
 
-//brute force Time complexity O(n^2) Space: O(1)
-var containsDuplicateBruteForce = function(nums) {
+//Brute force Time O(n^2) Space O(1)
+var containsDuplicate = function(nums) {
+    if(nums.length <= 1) return false;
+
     for(let i = 0; i < nums.length - 1; i++){
         for(let j = i + 1; j < nums.length; j++){
             if(nums[i] === nums[j]) return true;
@@ -16,9 +18,11 @@ var containsDuplicateBruteForce = function(nums) {
     return false;
 };
 
-//using sorting Time complexity O(nlogn) Space: O(1)
-var containsDuplicateBetter = function(nums) {
-    nums.sort((a,b) => a - b);
+//Sorting Time O(nlogn) Space O(1)
+var containsDuplicate = function(nums) {
+    if(nums.length <= 1) return false;
+
+    nums.sort((a, b) => a - b);
 
     for(let i = 1; i < nums.length; i++){
         if(nums[i] === nums[i - 1]) return true;
@@ -27,7 +31,21 @@ var containsDuplicateBetter = function(nums) {
     return false;
 };
 
-//using Set Time complexity O(n) Space: O(n)
+//Set Time O(n) Space O(n)
+var containsDuplicate = function(nums) {
+    if(nums.length <= 1) return false;
+
+   let set = new Set();
+
+   for(let num of nums){
+    if(set.has(num)) return true;
+    set.add(num);
+   }
+
+   return false;
+};
+
+//One liner Time O(n) Space O(n)
 var containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length;
 };
